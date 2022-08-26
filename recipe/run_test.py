a22b94f8824337467d19e0393365b3dc917913e7
Serhii Kupriienko <skupriienko@anaconda.com>
2022-08-26 08:56:49 +0000

    Add jupyter_client to test/requires

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,56 +1,51 @@
 import json
 import os
+import pkgutil
 import platform
 import sys
-import pkgutil
+import tempfile
+
 import pytest
 
-py_major = sys.version_info[0]
-py_impl = platform.python_implementation().lower()
-machine = platform.machine().lower()
 
+def go():
+    py_major = sys.version_info[0]
+    py_impl = platform.python_implementation().lower()
 
-print("Python implementation:", py_impl)
-print("              Machine:", machine)
-
-specfile = os.path.join(
-    os.environ["PREFIX"],
-    "share",
-    "jupyter",
-    "kernels",
-    "python{}".format(py_major),
-    "kernel.json",
-)
-
-print("Checking Kernelspec at:     ", specfile, "...\n")
-
-with open(specfile, "r") as fh:
-    raw_spec = fh.read()
-
-print(raw_spec)
-
-spec = json.loads(raw_spec)
-
-print("\nChecking python executable", spec["argv"][0], "...")
-
-if spec["argv"][0].replace("\\", "/") != sys.executable.replace("\\", "/"):
-    print(
-        "The kernelspec seems to have the wrong prefix. \n"
-        "Specfile: {}\n"
-        "Expected: {}"
-        "".format(spec["argv"][0], sys.executable)
+    print("Python implementation:", py_impl)
+    specfile = os.path.join(
+        os.environ["PREFIX"],
+        "share",
+        "jupyter",
+        "kernels",
+        "python{}".format(py_major),
+        "kernel.json",
     )
-    sys.exit(1)
 
-if py_impl == "pypy" and ("ppc" in machine or "aarch64" in machine):
-    print(f"Skipping pytest on {machine} for {py_impl}")
-    sys.exit(0)
+    print("Checking Kernelspec at:     ", specfile, "...\n")
 
-loader = pkgutil.get_loader("ipykernel.tests")
-pytest_args = [os.path.dirname(loader.path), "-vv", "--timeout", "300"]
+    with open(specfile, "r") as fh:
+        raw_spec = fh.read()
 
-if py_impl != "pypy":
-    # coverage is very slow on pypy
+    print(raw_spec)
+
+    spec = json.loads(raw_spec)
+
+    print("\nChecking python executable", spec["argv"][0], "...")
+
+    if spec["argv"][0].replace("\\", "/") != sys.executable.replace("\\", "/"):
+        print(
+            "The kernelspec seems to have the wrong prefix. \n"
+            "Specfile: {}\n"
+            "Expected: {}"
+            "".format(spec["argv"][0], sys.executable)
+        )
+        sys.exit(1)
+
+    loader = pkgutil.get_loader("ipykernel.tests")
+    pytest_args = [os.path.dirname(loader.path), "-vv", "--timeout", "300"]
+
+    # coverage options
     pytest_args += [
         "--cov",
         "ipykernel",
@@ -59,21 +54,24 @@ if py_impl != "pypy":
         "--no-cov-on-fail",
     ]
 
-# TODO: investigate upstream interrupt regression in 6.5.0
-skips = ["flaky", "interrupt"]
+    skips = ["flaky"]
 
-if len(skips) == 1:
-    pytest_args += ["-k", "not {}".format(*skips)]
-else:
-    pytest_args += ["-k", "not ({})".format(" or ".join(skips))]
+    if len(skips) == 1:
+        pytest_args += ["-k", "not {}".format(*skips)]
+    else:
+        pytest_args += ["-k", "not ({})".format(" or ".join(skips))]
 
-print("Final pytest args:", pytest_args)
+    print("Final pytest args:", pytest_args)
 
-# actually run the tests
-rc = pytest.main(pytest_args)
+    # actually run the tests
+    sys.exit(pytest.main(pytest_args))
 
-if json.loads(os.environ.get("MIGRATING", "0").lower()):
-    print("Ignoring pytest failure due to on-going migration...")
-    sys.exit(0)
 
-sys.exit(rc)
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        with tempfile.TemporaryDirectory() as appdata:
+            # prevent concurrent tests runs from overlapping Jupyter configs
+            os.environ["APPDATA"] = appdata
+            go()
+    else:
+        go()
